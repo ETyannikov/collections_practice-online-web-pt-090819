2424e5a24627839bd94b1f0ee79ee18b96403215
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-22 07:16:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,7 +49,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do {|a,index|}
+  array.each_with_index.collect do |a,index|
     if index == 1 
     else
       a << "s"
